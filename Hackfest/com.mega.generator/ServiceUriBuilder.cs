7c4a820adb8894b8d02d693d6de8a3390ccd6a24
mbruchet@mega.com <mbruchet@mega.com>
2017-06-07 13:36:25 +0100

    add service uri builder

---
+++ Hackfest/com.mega.generator/ServiceUriBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Fabric;
+
+namespace com.mega.generator
+{
+    public class ServiceUriBuilder
+    {
+        public string ApplicationInstance { get; set; }
+        public string ServiceInstance { get; set; }
+        public ICodePackageActivationContext ActivationContext { get; set; }
+
+
+        public ServiceUriBuilder(string serviceInstance)
+        {
+            ActivationContext = FabricRuntime.GetActivationContext();
+            ServiceInstance = serviceInstance;
+        }
+
+        public ServiceUriBuilder(ICodePackageActivationContext context, string serviceInstance)
+        {
+            ActivationContext = context;
+            ServiceInstance = serviceInstance;
+        }
+
+        public ServiceUriBuilder(ICodePackageActivationContext context, string applicationInstance, string serviceInstance)
+        {
+            ActivationContext = context;
+            ApplicationInstance = applicationInstance;
+            ServiceInstance = serviceInstance;
+        }
+
+        public Uri ToUri()
+        {
+            string applicationInstance = ApplicationInstance;
+
+            if (String.IsNullOrEmpty(applicationInstance))
+            {
+                // the ApplicationName property here automatically prepends "fabric:/" for us
+                applicationInstance = ActivationContext.ApplicationName.Replace("fabric:/", String.Empty);
+            }
+
+            return new Uri("fabric:/" + applicationInstance + "/" + ServiceInstance);
+        }
+    }
+}
